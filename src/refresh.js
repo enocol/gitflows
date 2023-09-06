74342945c4d775e9d709c645db688244e2a26111
Enoh <eno.col@gmail.com>
2023-09-06 20:49:10 +0100

    Create a JS module for refreshing data on the page

---
+++ src/refresh.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+const apiEndPoint =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HKlE8Ysavy1SaO4klV83/scores';
+
+const refresh = async () => {
+  const scoresList = document.querySelector('.scores-list');
+  const { data } = await axios.get(apiEndPoint);
+  scoresList.innerHTML = data.result
+    .map((element) => `<li>${element.user} : ${element.score}`)
+    .join(' ');
+};
+
+export default refresh;
