9b2c0204f45640de585c274a3b7df08290ed2943
Enoh <eno.col@gmail.com>
2023-09-08 10:36:28 +0100

    last commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import './styles.css';
 import addScore from './addscore.js';
 import getLocally from './getLocally.js';
 import refresh from './refresh.js';
+import testfile from './testfile.js';
 
 const addButton = document.querySelector('.submit');
 const refbtn = document.querySelector('.refresh');
+const footer = document.querySelector('.footer');
 
 addButton.addEventListener('click', addScore);
 refbtn.addEventListener('click', refresh);
+footer.innerHTML = testfile();
 
 window.onload = getLocally;
---
+++ src/testfile.js
@@ -0,0 +1,8 @@
+const testfile = () => {
+  const div = document.createElement('div');
+  div.className = 'class';
+  div.innerHTML = 'copy right Enoh Collins';
+  return div.innerHTML;
+};
+
+export default testfile;
